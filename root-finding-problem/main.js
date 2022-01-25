66790ca3c44139565e2c7b6967895fb277a3f9a9
Vattanak Keo <22200664+mulsannestraight@users.noreply.github.com>
2022-01-25 00:41:38 -0500

    Fix stylistic issues in main.js

--- root-finding-problem/main.js
+++ root-finding-problem/main.js
@@ -6,17 +6,21 @@ const cbrt = (x) => Math.cbrt(x);
 const PI = Math.PI;
 
 const printer = (object, name) => {
-  console.log(`${name} Method:`);
-  for(let i = 0; i < object.pn.length; i++) {
-    console.log(`p${i + 1} = ${object.pn[i]}`);
+  console.log(`${ name } Method:`);
+
+  for (let i = 0; i < object.pn.length; i++) {
+    console.log(`p${ i + 1 } = ${ object.pn[i] }`);
   }
+
   if (object.error) {
-    console.log(`Error: ${object.error}`);
+    console.log(`Error: ${ object.error }`);
   }
+
   if (object.approx) {
-    console.log(`Approximate solution: ${object.approx}`);
+    console.log(`Approximate solution: ${ object.approx }`);
   }
-  console.log(`Solution: ${object.solution}\n`);
+
+  console.log(`Solution: ${ object.solution }\n`);
 };
 
 const bisection = (a, b, tolerance, maxIterations, f) => {
@@ -32,11 +36,17 @@ const bisection = (a, b, tolerance, maxIterations, f) => {
     p = a + (b - a) / 2;
     pn.push(p);
     fp = f(p);
+
     if (fp === 0.0 || ((b - a) / 2) < tolerance) {
-      return {pn, "error": Math.abs(bn[bn.length - 1] - an[an.length - 1]),
-        "solution": "Found"};
+      return {
+        pn,
+        error: Math.abs(bn[bn.length - 1] - an[an.length - 1]),
+        solution: 'Found'
+      };
     }
+
     i++;
+
     if (fa * fp > 0) {
       a = p;
       an.push(a);
@@ -46,8 +56,12 @@ const bisection = (a, b, tolerance, maxIterations, f) => {
       bn.push(b);
     }
   }
-  return {pn, "error": Math.abs(bn[bn.length - 1] - an[an.length - 1]),
-    "solution": `Method failed after ${maxIterations} iterations`};
+
+  return {
+    pn,
+    error: Math.abs(bn[bn.length - 1] - an[an.length - 1]),
+    solution: `Method failed after ${ maxIterations } iterations`
+  };
 };
 
 const fixedPoint = (initialApprox, tolerance, maxIterations, g) => {
@@ -57,79 +71,101 @@ const fixedPoint = (initialApprox, tolerance, maxIterations, g) => {
 
   while (i <= maxIterations) {
     p = g(initialApprox);
+
     if (p) {
       pn.push(p);
     }
+
     if (Math.abs(p - initialApprox) < tolerance) {
-      return {pn, "approx": pn[pn.length - 1], "solution": "Found"};
+      return {
+        pn,
+        approx: pn[pn.length - 1],
+        solution: 'Found'
+      };
     }
+
     i++;
     initialApprox = p;
   }
-  return {pn, "approx": pn[pn.length - 1],
-    "solution": `Method failed after ${maxIterations} iterations.`};
+
+  return {
+    pn,
+    approx: pn[pn.length - 1],
+    solution: `Method failed after ${ maxIterations } iterations.`
+  };
 };
 
 const newton = (initialApprox, tolerance, maxIterations, f, fPrime) => {
   let i = 1;
   let p;
   let pn = [initialApprox];
-  
+
   while (i <= maxIterations) {
     p = initialApprox - (f(initialApprox) / fPrime(initialApprox));
+
     if (p) {
       pn.push(p);
     }
+
     if (Math.abs(p - initialApprox) < tolerance) {
-      return {pn, "approx": pn[pn.length - 1], "solution": "Found"};
+      return {
+        pn,
+        approx: pn[pn.length - 1],
+        solution: 'Found'
+      };
     }
+
     i++;
     initialApprox = p;
   }
-  return {pn, "approx": pn[pn.length - 1],
-    "solution": `Method failed after ${maxIterations} iterations.`};
+
+  return {
+    pn,
+    approx: pn[pn.length - 1],
+    solution: `Method failed after ${ maxIterations } iterations.`
+  };
 };
 
 let b1 = bisection(-0.9, 1, 0.0001, 15, (x) => {
-  return x*x*sin(x);
+  return x * x * sin(x);
 });
 let b2 = bisection(-0.9, 1, 0.0001, 15, (x) => {
-  return (x*x*sin(x) - x);
+  return (x * x * sin(x) - x);
 });
 let b3 = bisection(-0.9, 1, 0.0001, 15, (x) => {
   return cbrt(x);
 });
 let f1 = fixedPoint(0.1, 0.0001, 30, (x) => {
-  return (1/2)*(2*x+x*x*sin(x));
+  return (1 / 2) * (2 * x + x * x * sin(x));
 });
 let f2 = fixedPoint(0.1, 0.0001, 30, (x) => {
-  return x*x*sin(x);
+  return x * x * sin(x);
 });
 let f3 = fixedPoint(0.1, 0.0001, 30, (x) => {
-  return (1/2)*(cbrt(x) + 2*x);
+  return (1 / 2) * (cbrt(x) + 2 * x);
 });
 let n1 = newton(0.1, 0.0001, 10, (x) => {
-  return x*x*sin(x);
+  return x * x * sin(x);
 }, (x) => {
-  return (2*x*sin(x) + x*x*cos(x));
+  return (2 * x * sin(x) + x * x * cos(x));
 });
 let n2 = newton(0.1, 0.0001, 10, (x) => {
-  return x*x*sin(x) - x;
+  return x * x * sin(x) - x;
 }, (x) => {
-  return 2*x*sin(x) + x*x*cos(x) - 1;
+  return 2 * x * sin(x) + x * x * cos(x) - 1;
 });
 let n3 = newton(0.000001, 0.0001, 15, (x) => {
   return cbrt(x);
 }, (x) => {
-  return 1 / (3*cbrt(x*x));
+  return 1 / (3 * cbrt(x * x));
 });
 
-printer(b1, "Bisection");
-printer(f1, "Fixed-point");
-printer(n1, "Newton");
-printer(b2, "Bisection");
-printer(f2, "Fixed-point");
-printer(n2, "Newton");
-printer(b3, "Bisection");
-printer(f3, "Fixed-point");
-printer(n3, "Newton");
+printer(b1, 'Bisection');
+printer(f1, 'Fixed-point');
+printer(n1, 'Newton');
+printer(b2, 'Bisection');
+printer(f2, 'Fixed-point');
+printer(n2, 'Newton');
+printer(b3, 'Bisection');
+printer(f3, 'Fixed-point');
+printer(n3, 'Newton');
